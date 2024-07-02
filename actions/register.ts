9ee5c968f39bc9059097b78e279036f1d2af1e39
Bhavna Garg <bhavnagarg12ja@gmail.com>
2024-07-02 23:39:19 +0530

    login and register page ready

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,11 @@
 'use server';
-import { RegisterSchema } from '@/schemas';
+import bcrypt from 'bcrypt';
 import * as z from 'zod';
+
+import { RegisterSchema } from '@/schemas';
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/lib/db';
+
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
@@ -8,7 +13,18 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
       error: 'Invalid fields!',
     };
   }
-  return {
-    success: 'Successfully created!',
-  };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return { error: 'Email already taken' };
+  }
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  return { success: 'User created' };
 };
