adacb4f3eba0160da702afe022bb301ccf6f5062
Bhavna Garg <bhavnagarg12ja@gmail.com>
2024-07-04 23:37:52 +0530

    middleware

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import NextAuth from 'next-auth';
+import authConfig from '@/auth.config';
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  console.log('Route:', req.nextUrl.pathname);
+  console.log('Is LoggedIn: ', isLoggedIn);
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
