cf5876f7cd4f5af2f0b00c8ad554ea5e48ff01c0
Bhavna Garg <bhavnagarg12ja@gmail.com>
2024-07-10 23:45:49 +0530

    google and github authentication

---
+++ components/auth/error-card.tsx
@@ -0,0 +1,7 @@
+import { Header } from '@/components/auth/header';
+import { BackButton } from '@/components/auth/backButton';
+import { Card, CardFooter, CardHeader } from '@/components/ui/card';
+
+export const LoginForm = () => {
+  return <Card className="w-[400px] shadow-md"></Card>;
+};
