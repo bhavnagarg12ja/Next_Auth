9651ecfddefa403204f227f34de3405225b4493a
Bhavna Garg <bhavnagarg12ja@gmail.com>
2024-07-11 07:47:50 +0530

    Error Card added

--- components/auth/error-card.tsx
+++ components/auth/error-card.tsx
@@ -2,6 +2,15 @@ import { Header } from '@/components/auth/header';
 import { BackButton } from '@/components/auth/backButton';
 import { Card, CardFooter, CardHeader } from '@/components/ui/card';
 
-export const LoginForm = () => {
-  return <Card className="w-[400px] shadow-md"></Card>;
+export const ErrorCard = () => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label="Oops! Something went wrong!" />
+      </CardHeader>
+      <CardFooter>
+        <BackButton label="Back to login" href="/auth/login" />
+      </CardFooter>
+    </Card>
+  );
 };
